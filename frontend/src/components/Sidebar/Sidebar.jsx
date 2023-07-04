2e03db4e8adfd70f8b0412a9015f00217a0db446
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2023-07-04 14:49:48 -0300

    Estilos tela principal

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 
+import { BsPeopleFill } from 'react-icons/bs';
+import { MdOutlineFactory } from 'react-icons/md';
+import { AiOutlineCar } from "react-icons/ai";
+
 import './Sidebar.css';
 
 function Sidebar() {
   return (
     <div className='sidebar'>
-        <a href='#Clientes'>Clientes</a>
-        <a href='#Fornecedores'>Fornecedores</a>
-        <a href='#Serviços'>Serviços</a>
+        <a href='#Clientes' id='sidebar_id_clientes'> <BsPeopleFill /> Clientes</a>
+        <a href='#Fornecedores'> <MdOutlineFactory /> Fornecedores</a>
+        <a href='#Serviços'> <AiOutlineCar /> Serviços</a>
     </div>
   );
 }

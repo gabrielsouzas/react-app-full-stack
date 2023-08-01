384379e7972be066554a5c5619bfb835fa33d7a0
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2023-08-01 13:43:36 -0300

    Create authMiddleware.js

---
+++ backend/src/middlewares/authMiddleware.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const secretKey = process.env.SECRET_KEY;
+
+const verifyToken = (req, res, next) => {
+  const token = req.headers['authorization'];
+
+  if (!token) {
+    return res.status(401).json({ error: 'Token not provided.' });
+  }
+
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: 'Invalid token.' });
+    }
+
+    // Se o token é válido, você pode adicionar o usuário decodificado (ou informações relevantes) ao objeto 'req' para uso posterior
+    req.user = decoded;
+
+    // Chame o próximo middleware ou o controlador de rota, se o token for válido
+    next();
+  });
+};
+
+module.exports = verifyToken;
